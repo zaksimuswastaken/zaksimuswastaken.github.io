275e85586251350a8c3ec3f70e620350033a1be6
Z4KSIMUS <166195336+zaksimuswastaken@users.noreply.github.com>
2024-04-12 14:00:34 -0500

    pleasedontgo

---
+++ asciimedusa/pleasedontgo.js
@@ -0,0 +1,12 @@
+function showpleasedontgo() {
+    document.getElementById("pleasedontgo0").style.visibility = "visible";
+    document.getElementById("pleasedontgo1").style.visibility = "visible";
+    document.getElementById("pleasedontgo2").style.visibility = "visible";
+    document.getElementById("pleasedontgo3").style.visibility = "visible";
+    document.getElementById("pleasedontgo4").style.visibility = "visible";
+    document.getElementById("pleasedontgo5").style.visibility = "visible";
+    document.getElementById("pleasedontgo6").style.visibility = "visible";
+    document.getElementById("pleasedontgo7").style.visibility = "visible";
+    document.getElementById("pleasedontgo8").style.visibility = "visible";
+    document.getElementById("pleasedontgo9").style.visibility = "visible";
+}
